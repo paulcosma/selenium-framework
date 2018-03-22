f5fc7a080eb7b65bf3aa0aefc1bbc72327bc853a
Paul Cosma <paulcosma@gmail.com>
2018-03-22 19:15:43 +0200

    move before and after methods to baseTest

---
+++ src/main/java/base/BaseTest.java
@@ -0,0 +1,22 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+    protected WebDriver driver;
+
+    @BeforeMethod
+    protected void methodSetUp() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    protected void methodTearDown() {
+        driver.quit();
+    }
+}
--- src/test/java/com/paulcosma/tests/TestHomepage.java
+++ src/test/java/com/paulcosma/tests/TestHomepage.java
@@ -1,25 +1,9 @@
 package com.paulcosma.tests;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import base.BaseTest;
 import org.testng.annotations.Test;
 
-public class TestHomepage {
-    WebDriver driver;
-
-    @BeforeMethod
-    public void methodSetUp() {
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-    }
-
-    @AfterMethod
-    public void methodTearDown() {
-        driver.quit();
-    }
+public class TestHomepage extends BaseTest {
 
     @Test
     public void homepageLoaded() {
