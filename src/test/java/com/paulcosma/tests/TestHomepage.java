53bb47a58d230a464fc9d5c35451b0a886446b1b
Paul Cosma <paulcosma@gmail.com>
2018-03-22 19:00:45 +0200

    before and after method

--- src/test/java/com/paulcosma/tests/TestHomepage.java
+++ src/test/java/com/paulcosma/tests/TestHomepage.java
@@ -2,16 +2,28 @@ package com.paulcosma.tests;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TestHomepage {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void methodSetUp() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void methodTearDown() {
+        driver.quit();
+    }
 
     @Test
     public void homepageLoaded() {
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
         driver.get("https://paulcosma.com");
-        driver.quit();
     }
 
 }
