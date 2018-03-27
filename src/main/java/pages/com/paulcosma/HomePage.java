17acbb98e0083cccd62da88b97b24a69f25cb331
Paul Cosma <paulcosma@gmail.com>
2018-03-27 16:33:00 +0300

    create Base page Object

---
+++ src/main/java/pages/com/paulcosma/HomePage.java
@@ -0,0 +1,11 @@
+package pages.com.paulcosma;
+
+import base.BasePageObject;
+import org.openqa.selenium.WebDriver;
+
+public class HomePage extends BasePageObject<HomePage> {
+
+    protected HomePage(WebDriver driver) {
+        super(driver);
+    }
+}
