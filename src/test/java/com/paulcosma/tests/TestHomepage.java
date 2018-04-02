d46501554af4302b6e18af5e98459ed524db4178
Paul Cosma <paulcosma@gmail.com>
2018-04-02 14:03:03 +0300

    dataProvider

--- src/test/java/com/paulcosma/tests/TestHomepage.java
+++ src/test/java/com/paulcosma/tests/TestHomepage.java
@@ -1,9 +1,12 @@
 package com.paulcosma.tests;
 
 import base.BaseTest;
+import base.CsvDataProvider;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.com.paulcosma.HomePage;
+
+import java.util.Map;
 
 public class TestHomepage extends BaseTest {
 
@@ -16,20 +19,28 @@ public class TestHomepage extends BaseTest {
         homePage
                 .openHomePage()
                 .waitForHomePageToLoad();
-        String actualPageTitle = homePage.getTitle();
+//        String actualPageTitle = homePage.getTitle();
 //        Assert.assertTrue(expectedPageTitle.equals(actualPageTitle), "\nPage title does not match.\nExpected: " +
 //                expectedPageTitle + " \nActual: " + actualPageTitle + "\n");
 //        Assert.assertEquals(expectedPageTitle,actualPageTitle);
         Assert.assertTrue(homePage.isCorrectPageTitleDisplayed(expectedPageTitle), "Page title does not match");
     }
 
-    @Test
-    public void enterInvalidEmail() {
+    @Test(dataProvider = "CsvDataProvider", dataProviderClass = CsvDataProvider.class)
+    public void enterInvalidEmail(Map<String, String> testData) {
+        String testNumber = testData.get("no");
+        String email = testData.get("email");
+        String description = testData.get("description");
+
+        System.out.println("Test enterInvalidEmail number #" + testNumber + " with: " + description + " where \nEmail" +
+                " " +
+                "= " + email);
+
         HomePage homePage = new HomePage(driver);
         homePage
                 .openHomePage()
                 .waitForHomePageToLoad()
-                .enterEmail("invalid")
+                .enterEmail(email)
                 .clickGetNotification();
     }
 
