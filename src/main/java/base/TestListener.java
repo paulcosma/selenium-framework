80ce43cf925c648c91b78d8db52d7a67067dbbfa
Paul Cosma <paulcosma@gmail.com>
2018-04-03 21:39:24 +0300

    log for testListener

--- src/main/java/base/TestListener.java
+++ src/main/java/base/TestListener.java
@@ -1,28 +1,37 @@
 package base;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.testng.ITestContext;
 import org.testng.ITestResult;
 import org.testng.TestListenerAdapter;
 
 public class TestListener extends TestListenerAdapter {
+    public static Logger log = LogManager.getLogger(TestListener.class.getName());
 
     @Override
-    public void onTestSuccess(ITestResult var1) {
-        System.out.println(var1.getTestContext().getClass().getName() + "Test Success");
+    public void onTestSuccess(ITestResult testResult) {
+        log.info(testResult.getTestClass().getRealClass().getName() + " - "
+                + testResult.getMethod().getMethodName()
+                + " Test Success");
     }
 
     @Override
-    public void onTestFailure(ITestResult var1) {
-        System.out.println(var1.getTestContext().getName() + "Test Failure");
+    public void onTestFailure(ITestResult testResult) {
+        log.info(testResult.getTestClass().getRealClass().getName() + " - "
+                + testResult.getMethod().getMethodName()
+                + " Test Failure");
     }
 
     @Override
-    public void onStart(ITestContext var1) {
-        System.out.println(var1.getName() + "Test Start");
+    public void onStart(ITestContext testContext) {
+        log.info("Suite: " + testContext.getSuite().getName());
+        log.info(testContext.getName() + " Test Start");
     }
 
     @Override
-    public void onFinish(ITestContext var1) {
-        System.out.println(var1.getName() + "Test Finish");
+    public void onFinish(ITestContext testContext) {
+        log.info("Suite: " + testContext.getSuite().getName());
+        log.info(testContext.getName() + " Test Finish\n");
     }
 }
