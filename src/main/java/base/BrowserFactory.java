2b8931c8e4816422c0381ed5d42a69e3f8363e44
Paul Cosma <paulcosma@gmail.com>
2018-04-10 01:04:55 +0300

    env and browser as environment variables

--- src/main/java/base/BrowserFactory.java
+++ src/main/java/base/BrowserFactory.java
@@ -9,8 +9,12 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class BrowserFactory {
     public static Logger log = LogManager.getLogger(BrowserFactory.class.getName());
 
-    public static WebDriver getDriver(String browser) {
+    public static WebDriver getDriver(String browserParameter) {
         WebDriver driver = null;
+        String browser = System.getenv("browser");
+        if (browser == null) {
+            browser = browserParameter;
+        }
         log.debug("Starting " + browser + " driver");
         switch (browser) {
             case "chrome":
@@ -28,5 +32,28 @@ public class BrowserFactory {
         }
         driver.manage().window().maximize();
         return driver;
+    }
+
+    /**
+     * Get application environment
+     * Use first the environment variables
+     * If no environment variable is specified, use the defaultEnv
+     *
+     * @param defaultEnv
+     * @return
+     */
+    public static String getAppEnvironment(String defaultEnv) {
+        //get environment variable
+        String envValue = System.getenv("appenv");
+        //if environment variable is not specified use defaultEnv
+        if (envValue == null) {
+            envValue = defaultEnv;
+        }
+        //if production environment is specified leave variable empty, else add a dot a environment is a subdomain
+        if (envValue == "production") {
+            return "";
+        } else {
+            return envValue + ".";
+        }
     }
 }
--- src/main/java/data/com/paulcosma/Constants.java
+++ src/main/java/data/com/paulcosma/Constants.java
@@ -1,17 +1,21 @@
 package data.com.paulcosma;
 
+import base.BrowserFactory;
+
 public class Constants {
-    public final static String URL = "https://test.paulcosma.com/";
     public final static String HOMEPAGE_TITLE = "Paul Cosma";
     public final static String HELLO_MESSAGE = "Hello! We'll be ready soon ...";
     public final static String FOOTER_MESSAGE = "Powered by Bootstrap";
-
     public final static String EXTERNAL_LINK_FACEBOOK = "https://www.facebook.com/paulcosma";
     public final static String EXTERNAL_LINK_TWITTER = "https://twitter.com/PaulPCosma";
     public final static String EXTERNAL_LINK_GITHUB = "https://github.com/paulcosma";
     public final static String EXTERNAL_LINK_LINKEDIN = "https://www.linkedin.com/in/paulpcosma/";
     public final static String EXTERNAL_LINK_BOOTSTRAP = "http://getbootstrap.com/";
-
     public final static String EMAIL_ALERT_VALID = "Valid email. Thank you!";
     public final static String EMAIL_ALERT_INVALID = "Email not valid";
+    //Links
+    public static String defaultEnv = "test";
+    public static String protocol = "https://";
+    public static String domain = "paulcosma.com";
+    public static String URL = protocol + BrowserFactory.getAppEnvironment(defaultEnv) + domain;
 }
